08274103450d71bbb0c6adbfd003a505be522067
arun <arunsrajan@gmail.com>
2023-03-08 22:40:40 +0530

    HawtIO for monitoring Camel

--- springbootexample/scripts/provisionkafka.sh
+++ springbootexample/scripts/provisionkafka.sh
@@ -1,6 +1,6 @@
 docker run -d --name zookeeper --network redis_redisnet -e ALLOW_ANONYMOUS_LOGIN=yes bitnami/zookeeper:latest
 docker run -d --network redis_redisnet --name kafka -e ALLOW_PLAINTEXT_LISTENER=yes -e KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper:2181 bitnami/kafka:latest
 docker run --name activemq --network redis_redisnet -p 61616:61616 -p 8161:8161 -d rmohr/activemq
-docker run --network redis_redisnet --name springboot1 -p 1090:1090 -v "C:/DEVELOPMENT:/DEVELOPMENT" -d springbootcamel
-docker run --network redis_redisnet --name springboot2 -p 1091:1090 -v "C:/DEVELOPMENT:/DEVELOPMENT" -d springbootcamel
-docker exec -it redis-redis-1-1 redis-cli --cluster create redis-1:6379 redis-2:6379 redis-3:6379 redis-4:6379 redis-5:6379 redis-6:6379 --cluster-replicas 6 --cluster-yes
+docker run --network redis_redisnet --name springboot1 -P -v "C:/DEVELOPMENT:/DEVELOPMENT" -d springbootcamel
+docker run --network redis_redisnet --name springboot2 -P -v "C:/DEVELOPMENT:/DEVELOPMENT" -d springbootcamel
+docker exec -it redis-redis1-1 redis-cli --cluster create redis1:6379 redis2:6379 redis3:6379 redis4:6379 redis5:6379 redis6:6379 --cluster-replicas 1 --cluster-yes
